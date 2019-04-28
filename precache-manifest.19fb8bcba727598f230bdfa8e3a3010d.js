552dea33c96ee7e3e886cb741bc5f5cc614b57ed
Cameron Williams <cameronrw.nz@gmail.com>
2019-04-28 13:05:00 +0700

    Initial dist subtree commit

---
+++ precache-manifest.19fb8bcba727598f230bdfa8e3a3010d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Todo-List/robots.txt"
+  },
+  {
+    "revision": "8da08b2cdc94c669c1b9",
+    "url": "/Todo-List/js/chunk-vendors.6c9b487c.js"
+  },
+  {
+    "revision": "523f48bf6756c16a03ef",
+    "url": "/Todo-List/js/app.e3a63f71.js"
+  },
+  {
+    "revision": "20739b4cabf49e2574f15403c515f468",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "523f48bf6756c16a03ef",
+    "url": "/Todo-List/css/app.134536fa.css"
+  }
+];
